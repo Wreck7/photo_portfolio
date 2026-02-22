2526c319cec61b6c882bdf362f8bd5e7a452ec91
Vishwa <vishwagovula07@gmail.com>
2026-02-22 22:33:14 +0530

    snowfall effect added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Snowfall from 'react-snowfall';
 import HomePage from '@/pages/HomePage';
 
 function App() {
   return (
     <BrowserRouter>
+      {/* Snowfall overlay */}
+      <Snowfall
+        color="#ffffff7a"
+        snowflakeCount={120}
+        speed={[0.5, 1]}
+        wind={[-0.5, 0.5]}
+        style={{
+          position: "fixed",
+          width: "100vw",
+          height: "100vh",
+          zIndex: 1,
+          pointerEvents: "none",
+        }}
+      />
+
       <Routes>
         <Route path="/" element={<HomePage />} />
       </Routes>
